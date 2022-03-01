d98e3a8d8cb69c6e9c266366ff666c313d0f0b39
OgunniranRuth <iyaboogunniran071316@yahoo.com>
2022-03-01 12:17:42 +0100

    mappingcomment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import { deselectOptions } from "@testing-library/user-event/dist/select-options";
+import React, {useState} from "react";
 import './App.css';
 
+
+
+
 function App() {
+  const[text, settext] = useState("");
+  const [lsetf, setlsetf] = useState([]);
+  
+  const post = () =>{
+    const items = {
+      id : lsetf.length +1,
+      comment: text,
+    };
+    setlsetf([...lsetf, items]);
+    settext("");
+  };
+
+  const Delete = ()=>{
+    const items={
+      id:"",
+      comment:"",
+    }
+  }
+
+
+
+  console.log(lsetf)
+  console.log(text)
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="cardholder">
+      <br/>
+      <span><input  classname= "input" value= {text} onChange={(e)=>{settext(e.target.value)
+      console.log(text)
+      }}placeholder="pholder"/>
+      <br/>
+      <button className="post" onClick={()=>{ post()
+        console.log('Have Posted')
+      }}> post</button></span>
+
+
+      {lsetf.map((props)=>(
+        <div className="Card">
+          <div> {props.id}.</div> 
+          <div> Comment:{props.comment} <button className="btn" 
+          onClick={()=>{Delete()}}> DELETE</button></div>
+        </div>
+      ))}
     </div>
-  );
+  )
 }
 
 export default App;
